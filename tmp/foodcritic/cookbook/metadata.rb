eaae92c60a9b42fcc1706aa66589f5ee72d62bec
Sam Dunne <sam@sam-dunne.com>
2014-11-21 15:08:03 +0000

    Removing files I shouldn't have stored

--- tmp/foodcritic/cookbook/metadata.rb
+++
@@ -1,7 +0,0 @@
-name             'jenkins_packer'
-maintainer       'otherworld'
-maintainer_email ['sam@sam-dunne.com' 'davedcusack@gmail.com']
-license          'Apache 2.0'
-description      'A Jenkins Pipeline for building images with Packer'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
--- tmp/foodcritic/cookbook/recipes/default.rb
+++
@@ -1,6 +0,0 @@
-#
-# Cookbook Name:: jenkins_packer
-# Recipe:: default
-#
-# Copyright 2014, Otherworld
-#
