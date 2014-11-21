79e5153c45f5dd2fc3a7e8160bde3ed57d301fcb
Sam Dunne <sam@sam-dunne.com>
2014-11-21 15:10:28 +0000

    Fix maintainer email

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
 name             'jenkins_packer'
 maintainer       'otherworld'
-maintainer_email ['sam@sam-dunne.com' 'davedcusack@gmail.com']
+maintainer_email 'sam@sam-dunne.com'
 license          'Apache 2.0'
 description      'A Jenkins Pipeline for building images with Packer'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
